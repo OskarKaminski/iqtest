7d2e23627754c5acab685439d517b78c526cdb31
OskarKaminski <oskarkaminski.dev@gmail.com>
2017-05-14 20:42:21 +0200

    Test cases list updated

--- app/Components/Progress/Progress.js
+++ app/Components/Progress/Progress.js
@@ -4,12 +4,7 @@ import './Progress.scss'
 export const Progress = (props) => (
     <div className="progress-wrapper">
         <div className="progress">
-            <span className="progress__question-no">
-                {props.questionNo}
-            </span> /
-                <span className="progress__questions-qty">
-                {props.questionsQty}
-            </span>
+            {props.questionNo + '/' + props.questionsQty}
         </div>
     </div>
 )
--- app/testCases.js
+++ app/testCases.js
@@ -23,27 +23,10 @@ export default [{
     },
     {
         number: 3,
-        question: {
-            ctx: '2, 4, 6, 8,',
-            text: 'What is the next number?'
-        },
-        options: [
-            {label: 9},
-            {label: 10},
-            {label: 12},
-            {label: 8}
-        ]
-    },
-    {
-        number: 3,
+        type: 'list',
         question: {
             ctx: '3, 5, 9, 17,',
             text: 'What is the next number?'
         },
-        options: [
-            {label: 40},
-            {label: 20},
-            {label: 33},
-            {label: 54}
-        ]
+        options: [21, 27, 33, 40]
     }]
