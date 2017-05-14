182258c2146174233bf5a9ba533cbfdc76b017dd
OskarKaminski <oskarkaminski.dev@gmail.com>
2017-05-14 16:47:38 +0200

    OnEnter puzzle

--- app/Components/Puzzle/Puzzle.js
+++ app/Components/Puzzle/Puzzle.js
@@ -1,29 +1,45 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {nextTestCase} from 'Actions'
 import './Puzzle.scss'
 
 const changer = (number) => {
-    return number || <input className="changer_button" type="text" placeholder="?"/>
+    return number || <input className="changer_button"
+                            type="text"
+                            placeholder="?"/>
 }
 
+@connect(undefined, {nextTestCase})
+export class Puzzle extends React.Component{
+    constructor (props){
+        super(props)
+    }
+    
+    onEnter(e){
+        if(e.keyCode === 13){
+            this.props.nextTestCase()
+        }
+    }
 
-export const Puzzle = (props) => {
-    return (
-        <div className="puzzle">
-            <div className="puzzle__element firstcolumn">
-                {changer(props.one)}
+    render(){
+        return (
+            <div className="puzzle" onKeyDown={this.onEnter.bind(this)}>
+                <div className="puzzle__element firstcolumn">
+                    {changer(this.props.one)}
+                </div>
+                <div className="puzzle__element secondcolumn">
+                    {changer(this.props.two)}
+                </div>
+                <div className="puzzle__element thridcolumn">
+                    {changer(this.props.three)}
+                </div>
+                <div className="puzzle__element fourthcolumn">
+                    {changer(this.props.four)}
+                </div>
+                <div className="puzzle__element lastcolumn">
+                    {changer(this.props.five)}
+                </div>
             </div>
-            <div className="puzzle__element secondcolumn">
-                {changer(props.two)}
-            </div>
-            <div className="puzzle__element thridcolumn">
-                {changer(props.three)}
-            </div>
-            <div className="puzzle__element fourthcolumn">
-                {changer(props.four)}
-            </div>
-            <div className="puzzle__element lastcolumn">
-                {changer(props.five)}
-            </div>
-        </div>
-    )
+        )
+    }
 }
