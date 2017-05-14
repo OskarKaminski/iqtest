d2a701307f0a5d0ed0ae1fbd0c22c23a9a67b035
OskarKaminski <oskarkaminski.dev@gmail.com>
2017-05-14 16:00:37 +0200

    testCases moved out to external file

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Provider, connect} from 'react-redux';
 import {store} from './store';
+import testCases from './testCases';
 import {setTestCases} from './Actions/actions'
 import {TestCase} from 'C/TestCase/TestCase'
 import {Progress} from 'C/Progress/Progress'
@@ -16,58 +17,20 @@ class App extends React.Component {
     }
 
     componentDidMount () {
-        this.props.setTestCases([{
-            number: 1,
-            question: {
-                ctx: '1, 2, 3, 4, 5,',
-                text: 'What is the next number?'
-            },
-            answers: [
-                {label: 3},
-                {label: 10},
-                {label: 6},
-                {label: 100}
-            ]
-        },
-            {
-                number: 2,
-                question: {
-                    ctx: '2, 4, 6, 8,',
-                    text: 'What is the next number?'
-                },
-                answers: [
-                    {label: 9},
-                    {label: 10},
-                    {label: 12},
-                    {label: 8}
-                ]
-            },
-            {
-                number: 3,
-                question: {
-                    ctx: '3, 5, 9, 17,',
-                    text: 'What is the next number?'
-                },
-                answers: [
-                    {label: 40},
-                    {label: 20},
-                    {label: 33},
-                    {label: 54}
-                ]
-            }]);
+        this.props.setTestCases(testCases);
     }
 
-    renderTestCase = () => {
+    renderTestCase = (testCases) => {
         return this.props.testCases[0] ?
-            <ThreeTestsHolder tests={this.props.testCases}
+            <ThreeTestsHolder tests={testCases}
                               current={this.props.currentTestCase}/> : null
     }
 
     render = () => (
         <div>
             <Progress questionNo={this.props.currentTestCase}
-                      questionsQty={15}/>
-            {this.renderTestCase()}
+                      questionsQty={this.props.testCases.length}/>
+            {this.renderTestCase(this.props.testCases)}
         </div>
     )
 }
---
+++ app/testCases.js
@@ -0,0 +1,39 @@
+export default [{
+    number: 1,
+    question: {
+        ctx: '1, 2, 3, 4, 5,',
+        text: 'What is the next number?'
+    },
+    answers: [
+        {label: 3},
+        {label: 10},
+        {label: 6},
+        {label: 100}
+    ]
+},
+    {
+        number: 2,
+        question: {
+            ctx: '2, 4, 6, 8,',
+            text: 'What is the next number?'
+        },
+        answers: [
+            {label: 9},
+            {label: 10},
+            {label: 12},
+            {label: 8}
+        ]
+    },
+    {
+        number: 3,
+        question: {
+            ctx: '3, 5, 9, 17,',
+            text: 'What is the next number?'
+        },
+        answers: [
+            {label: 40},
+            {label: 20},
+            {label: 33},
+            {label: 54}
+        ]
+    }]
